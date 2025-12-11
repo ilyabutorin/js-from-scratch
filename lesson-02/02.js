21fdb3c530c4c0c132040d9296ec281293d8dbe0
Ilya Butorin <butorynmi@gmail.com>
2025-12-11 17:50:44 +0200

    lesson02-02 is done

--- lesson-02/02.js
+++ lesson-02/02.js
@@ -7,16 +7,23 @@
 * Для успешного прохождения тестов не меняйте названия переменных!
 */
 
-let dice1 // your code
-let dice2 // your code
-let isWinningDouble // your code
 
-/*
-console.log('Первый бросок: ' + dice1)
-console.log('Второй бросок: ' + dice2)
-if (isWinningDouble) {
-  console.log('Выигрышный дубль!')
-} else {
-  console.log('Не выигрышный дубль.')
-}
-*/
+let isWinningDouble = false;
+
+while (isWinningDouble === false) {
+
+  let dice1 = Math.floor(Math.random() * 6) + 1;
+  let dice2 = Math.floor(Math.random() * 6) + 1;
+
+  if (dice1 === dice2 && dice1 > 3 && dice2 > 3) {
+    isWinningDouble = true;
+  }
+
+  console.log('Первый бросок: ' + dice1)
+  console.log('Второй бросок: ' + dice2)
+  if (isWinningDouble) {
+    console.log('Выигрышный дубль!')
+  } else {
+    console.log('Не выигрышный дубль.')
+  }
+};
