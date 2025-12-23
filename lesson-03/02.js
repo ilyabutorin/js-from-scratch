29192d8538701ad31965baf073bfd1071d614bd0
Ilya Butorin <butorynmi@gmail.com>
2025-12-23 14:26:54 +0200

    lesson03 second task is done

--- lesson-03/02.js
+++ lesson-03/02.js
@@ -1,4 +1,7 @@
 // Разработайте функцию isEven, которая определяет, чётное ли число.
 // Функция должна возвращать true, если число чётное, и false — в противном случае
 
-function isEven() {}
+function isEven(digit){
+    return digit = true ? digit % 2 === 0 : false;
+};
+isEven();
