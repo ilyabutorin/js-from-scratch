39136650f67c7b10a46e5a14050c4a21842dfc14
Ilya Butorin <butorynmi@gmail.com>
2025-12-23 18:02:33 +0200

    changes and improvements in 3rd lesson

--- lesson-03/01.js
+++ lesson-03/01.js
@@ -3,4 +3,4 @@
 function calculateAverage(num1, num2){
     return (num1 + num2) / 2
 };
-calculateAverage();
+calculateAverage(4, 11);
--- lesson-03/02.js
+++ lesson-03/02.js
@@ -2,6 +2,6 @@
 // Функция должна возвращать true, если число чётное, и false — в противном случае
 
 function isEven(digit){
-    return digit = true ? digit % 2 === 0 : false;
+    return digit % 2 === 0
 };
-isEven();
+isEven(5);
